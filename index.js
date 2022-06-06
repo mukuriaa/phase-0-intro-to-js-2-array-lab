e51cbc983273e952e889e1a2a4f5061f9bc38d47
Eric Mukuria <mukuriaeric@gmail.com>
2022-06-06 02:01:10 -0700

    commit tree 414d49bda7a09fb820f70a29e79b3853751fa555

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+ cats.push("Ralph")
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop("Milo","Otis")
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    var newList = [...cats, name]; 
+    return newList;
+}
+
+function prependCat(name){
+    var newList = [name, ...cats]; 
+    return newList;
+}
+
+function removeLastCat(){
+    var newList = cats.slice(0,cats.length-1);
+    return newList
+}
+
+function removeFirstCat(){
+    var newList = cats.slice(1);
+    return newList
+}
